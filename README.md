123214cbcfe41de214e979cff359507ab43d60e9
Sean O <seanodotcom@yahoo.com>
2024-03-02 09:55:03 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# reactjs-todolist
+ ReactJS Tutorial from @Smoljames
---
+++ src/components/TodoInput.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+export default function TodoInput(props) {
+  const { handleAddTodos, todoValue, setTodoValue } = props;
+
+  function handleKeyDown(e) {
+    if (e.key === "Enter") {
+      handleAddTodos(todoValue);
+      setTodoValue("");
+    }
+  }
+
+  return (
+    <header>
+      <input
+        value={todoValue}
+        onChange={(e) => {
+          setTodoValue(e.target.value);
+        }}
+        onKeyDown={handleKeyDown}
+        placeholder="enter todo..."
+      />
+      <button
+        onClick={() => {
+          handleAddTodos(todoValue);
+          setTodoValue("");
+        }}
+      >
+        Add
+      </button>
+    </header>
+  );
+}
